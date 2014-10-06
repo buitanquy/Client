c544d1b09c970f036c85f9996f0f2482f96a151b
Bùi Tấn Quý <buitanquy@outlook.com.vn>
2014-10-06 09:21:18 +0700

    MovingSocial_Start

---
+++ Moving Social/Moving Social/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Moving_Social.Resources;
+
+namespace Moving_Social
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ Moving Social/Moving Social/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Moving_Social.Resources;
+
+namespace Moving_Social
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Signin_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/MapPage.xaml", UriKind.Relative));
+        }
+
+    }
+}
